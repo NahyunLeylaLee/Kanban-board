d640e6fbc6859dbfacbdc9f2bc04b564691277ce
NahyunLeylaLee <florence5107@gmail.com>
2025-07-28 14:05:37 -0700

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import KanbanBoard from './components/KanbanBoard';
 function App() {
   return (
     <div className="App">
+      <title>Kanban Board</title>
       <KanbanBoard />
     </div>
   );
