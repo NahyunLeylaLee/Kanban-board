299135effa359127c4f47e2f7e6730bb4e0ea8d5
NahyunLeylaLee <florence5107@gmail.com>
2025-08-04 23:35:52 -0700

    add recoild

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { RecoilState, useRecoilValue } from 'recoil';
 import KanbanBoard from './KanbanBoard';
+import { isDarkAtom } from './atoms';
 
 function App() {
+  const isDark = useRecoilValue(isDarkAtom);
   return (
     <div className="App">
       <title>Kanban Board</title>
